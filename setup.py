7e0ffd33c4adfccfa5667b5c389153990734e09c
vladprmk <prmkvlad.work@gmail.com>
2025-04-01 19:14:06 +0300

    chore: update `setup.py`

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MyProject",
+    version="0.1.0",
+    description="A simple command-line To-Do task manager",
+    author="prmkvlad",
+    packages=find_packages(),
+    install_requires=[
+        "pytest>=7.0.0",
+        "flake8>=6.0.0",
+        "black>=23.0.0",
+    ],
+    python_requires=">=3.6",
+)
