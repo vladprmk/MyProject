c0dd007ca1e44eef1d97f52cb4b86f186817a551
vladprmk <prmkvlad.work@gmail.com>
2025-04-01 22:24:02 +0300

    ci: ignore missing coverage.xml in SonarQube job

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         "pytest>=7.0.0",
         "flake8>=6.0.0",
         "black>=23.0.0",
-        "coverage"
+        "coverage",
+        "pytest-cov"
     ],
     python_requires=">=3.6",
 )
